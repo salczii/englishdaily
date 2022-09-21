c8442ceb1f79b210b405434f767f718b448c1cb7
Kacper Sałek <kacpersalek@Mac-Studio-Kacper.local>
2022-09-21 23:10:21 +0200

    repair mainlayout by swapping margin to padding property

--- components/MainLayout.tsx
+++ components/MainLayout.tsx
@@ -6,7 +6,7 @@ const MainLayout = ({ children }) => {
       <Box position="absolute" top="0" left="0" height="75px" width="100vw">
         Navigation
       </Box>
-      <Box width="100vw" marginTop="75px" height="calc(100vh-75px-100px)">
+      <Box width="100vw" paddingTop="75px" height="calc(100vh-75px-100px)">
         {children}
       </Box>
       <Box bottom="0" left="0" height="100px" position="absolute">
